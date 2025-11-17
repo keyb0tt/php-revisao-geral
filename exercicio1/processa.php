b0c0c9c864bdba61b186e48b0d5df4acb3117823
kaicbosco <kaiqueboscoaraujo00@gmail.com>
2025-11-17 13:01:59 -0300

    .

---
+++ exercicio1/processa.php
@@ -0,0 +1,11 @@
+<!-- Exercício 1: Formulário Básico de Cadastro
+Crie um formulário HTML para cadastrar nome, email e idade. No PHP, valide se todos os campos foram preenchidos e exiba os dados. -->
+<?php
+
+
+
+
+
+
+
+?>
